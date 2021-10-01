361a8e3ad1a3d4bf291c4a6c8d05cb9286527a47
Arifulislamt <arifulislamt06@gmail.com>
2021-10-01 15:19:26 +0600

    updated the inventory page

---
+++ src/components/Invantory/Invabtory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Invabtory = () => {
+    return (
+        <div>
+            <h2>This is Inventory</h2>
+        </div>
+    );
+};
+
+export default Invabtory;
